0e3412e617c83f6cc7a50e593e5427ec4ebdaea0
Kalindu Kumarasinghe <Kalindu Kumarasinghe>
2017-03-02 10:29:40 +0530

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kalindu on 3/2/2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        main main =new main();
+        person pers= new person();
+        pers.setName("Kuma");
+        pers.setAge(21);
+
+    }
+}
